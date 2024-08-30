73d379fcc82c04c935d2a794ab1589ac11603c4f
Erwin Mareto Wikas <erwinmaretow@gmail.com>
2024-08-30 15:57:02 +0700

    working with FE

--- app.js
+++ app.js
@@ -1,24 +1,27 @@
-const express = require('express');
-const cookieParser = require('cookie-parser');
-const authRoutes = require('./routes/authRoutes');
-const userRoutes = require('./routes/userRoutes');
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const authRoutes = require("./routes/authRoutes");
+const userRoutes = require("./routes/userRoutes");
 
 const app = express();
+const cors = require("cors");
 const port = process.env.PORT || 3000;
+
+app.use(cors());
 
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(express.static('public'));
-app.use('/uploads', express.static('uploads'));
+app.use(express.static("public"));
+app.use("/uploads", express.static("uploads"));
 
-app.use('/api/auth', authRoutes);
-app.use('/api/user', userRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/user", userRoutes);
 
-app.get('/', (req, res) => {
-    res.send('Reminderoo API');
+app.get("/", (req, res) => {
+  res.send("Reminderoo API");
 });
 
 app.listen(port, () => {
-    console.log(`Reminderoo server running on port ${port}`);
+  console.log(`Reminderoo server running on port ${port}`);
 });
--- models/user.js
+++ models/user.js
@@ -1,49 +1,62 @@
-const mysql = require('mysql2/promise');
-const bcrypt = require('bcrypt');
-const pool = require('../db');
+const mysql = require("mysql2/promise");
+const bcrypt = require("bcrypt");
+const pool = require("../db");
 
 class User {
-    static async create(username, name, email, password) {
-        const hashedPassword = await bcrypt.hash(password, 10);
-        const [result] = await pool.query(
-        'INSERT INTO users (username, name, email, password) VALUES (?, ?, ?, ?)',
-        [username, name, email, hashedPassword]
-        );
-        const defaultProfilePicture = `../public/images/default_profiles/default_${Math.floor(Math.random() * 5) + 1}.jpg`;
-        return result.insertId;
-    }
+  static async create(username, name, email, password) {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const [result] = await pool.query(
+      "INSERT INTO users (username, name, email, password) VALUES (?, ?, ?, ?)",
+      [username, name, email, hashedPassword]
+    );
+    const defaultProfilePicture = `../public/images/default_profiles/default_${
+      Math.floor(Math.random() * 5) + 1
+    }.jpg`;
+    return result.insertId;
+  }
 
-    static async findByEmail(email) {
-        const [rows] = await pool.query('SELECT * FROM users WHERE email = ?', [email]);
-        return rows[0];
-    }
+  static async findByEmail(email) {
+    const [rows] = await pool.query("SELECT * FROM users WHERE email = ?", [
+      email,
+    ]);
+    return rows[0];
+  }
 
-    static async findByUsername(username) {
-        const [rows] = await pool.query('SELECT * FROM users WHERE username = ?', [username]);
-        return rows[0];
-    }
+  static async findByUsername(username) {
+    const [rows] = await pool.query("SELECT * FROM users WHERE username = ?", [
+      username,
+    ]);
+    return rows[0];
+  }
 
+  static async updateProfilePicture(userId, profilePicture) {
+    await pool.query("UPDATE users SET profile_picture = ? WHERE id = ?", [
+      profilePicture,
+      userId,
+    ]);
+  }
 
-    static async updateProfilePicture(userId, profilePicture) {
-        await pool.query('UPDATE users SET profile_picture = ? WHERE id = ?', [profilePicture, userId]);
-    }
+  static async findById(userId) {
+    const [rows] = await pool.query("SELECT * FROM users WHERE id = ?", [
+      userId,
+    ]);
+    return rows[0];
+  }
 
-    static async findById(userId) {
-        const [rows] = await pool.query('SELECT * FROM users WHERE id = ?', [userId]);
-        return rows[0];
-    }
+  static async updateProfile(userId, { name, email, phoneNumber }) {
+    await pool.query(
+      "UPDATE users SET name = ?, email = ?, phone_number = ? WHERE id = ?",
+      [name, email, phoneNumber, userId]
+    );
+    return this.findById(userId);
+  }
 
-    static async updateProfile(userId, { name, email, phoneNumber }) {
-        await pool.query(
-            'UPDATE users SET name = ?, email = ?, phone_number = ? WHERE id = ?',
-            [name, email, phoneNumber, userId]
-        );
-        return this.findById(userId);
-    }
-
-    static async updatePassword(userId, newHashedPassword) {
-        await pool.query('UPDATE users SET password = ? WHERE id = ?', [newHashedPassword, userId]);
-    }
+  static async updatePassword(userId, newHashedPassword) {
+    await pool.query("UPDATE users SET password = ? WHERE id = ?", [
+      newHashedPassword,
+      userId,
+    ]);
+  }
 }
 
 module.exports = User;
