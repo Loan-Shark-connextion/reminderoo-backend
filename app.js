39a6afbf8bd6b4a3e27dd936896406676ab654ae
Hadi <hadimuhammadrahman@gmail.com>
2024-10-04 12:33:20 +0700

    cors

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cookieParser());
 
+app.use((cors()));
 app.use(express.static('public'));
 app.use('/uploads', express.static('uploads'));
 
